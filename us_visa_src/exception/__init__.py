8e630f430d6d85b3a7c3690bbbfee0e6b926172d
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2024-06-09 11:24:42 +0530

    custom exception code done

---
+++ us_visa_src/exception/__init__.py
@@ -0,0 +1,29 @@
+
+import sys
+
+class USvisaException(Exception):
+    def __init__(self,error_message:Exception,error_details:sys):
+        super().__init__()
+        self.error_details=error_details
+        self.error_message= USvisaException.get_error_message(error_message=error_message,
+                                                              error_details=self.error_details)
+    
+    @staticmethod
+    def get_error_message(error_message:Exception,error_details:sys):
+        _,__,exc_tb=error_details.exc_info()
+        file_name=exc_tb.tb_frame.f_code.co_filename
+        line_number=exc_tb.tb_lineno
+        
+        complete_error_message=f""" 
+        Exception occurred in script file : {file_name}
+            line number : {line_number}
+            error message is : {str(error_message)}
+        """
+        
+        return complete_error_message
+        
+    def __str__(self) -> str:
+        return self.error_message
+    
+    def __repr__(self) -> str:
+        return self.error_message
